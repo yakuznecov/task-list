7b8cccffa6165a1a2d9a67d74ed3f71321b70f46
yakuznecov <yakuznecov@mail.ru>
2022-08-18 22:12:21 +0300

    add tasks

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import styled from 'styled-components';
 
-import './Button.css';
+const Button = styled.button`
+	font: inherit;
+	padding: 0.5rem 1.5rem;
+	color: white;
+	border: none;
+	background: yellowgreen;
+	box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+	cursor: pointer;
 
-const Button = (props) => {
-	return (
-		<button type={props.type} className='button' onClick={props.onClick}>
-			{props.children}
-		</button>
-	);
-};
+	&:focus {
+		outline: none;
+	}
+
+	&:hover,
+	&:active {
+		background: rgb(114, 153, 37);
+		border-color: rgb(114, 153, 37);
+		box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+	}
+`;
+
+// const Button = (props) => {
+// 	return (
+// 		<button type={props.type} className='button' onClick={props.onClick}>
+// 			{props.children}
+// 		</button>
+// 	);
+// };
 
 export default Button;
